46f96f76d9dc59a886450d5a06b7b16277db4013
AnjPR <anjanaprmail@gmail.com>
2024-07-10 16:14:40 +0530

    ...

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/name",methods =['GET', 'POST'])
+@app.route("/",methods =['GET', 'POST'])
 
 def home():
     if request.method == "POST":
         input1 = request.form.get('name')
         input2 = request.form.get('message')
         
-        return render_template('name.html', Result=input1,name=input2)
+        return render_template('name.html', Result=input1,message=input2)
     
-    return render_template('homepage.html',name="GreetingU")
+    return render_template('homepage.html')
 
 
 
