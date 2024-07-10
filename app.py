0302ef8e8b995bfd51356c1f6dbd484d6fe9ee39
AnjPR <anjanaprmail@gmail.com>
2024-07-10 14:29:56 +0530

    ...

--- app.py
+++ app.py
@@ -1,49 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+
+
 app = Flask(__name__)
-@app.route("/",methods =['GET', 'POST'])
-def home():
-    return render_template('homepage.html')
 
 @app.route("/name",methods =['GET', 'POST'])
 
 def home():
+    if request.method == "POST":
+        input1 = request.form.get('name')
+        input2 = request.form.get('message')
+        
+        return render_template('name.html', Result=input1,name=input2)
+    
     return render_template('homepage.html',name="GreetingU")
 
-@app.route("/name",methods =['GET', 'POST'])
-
-try:
-    def operation():
-         first_input=request.form['Name']
-         greet=request.form['Greeting']
 
 
-         input=string(first_input)
-         if operation=="1":
-              Result="Happy birthday"
-         elif operation=="2":
-              Result="Happy Christmas"
-         else:
-              operation="3"
-              Result="Happy Newyear"
-    
-    return render_template(
-       'name.html',
-       input1=input1,
-       result=Result,
-       calculation_success=True
-    )
-
-except UnknownError:
-    return render_template(
-       'name.html',
-       input1=input1,
-       result="Unknown Error",
-       calculation_success=False
-    )
-   
-    
-    
-    
-if _name_='_main_':
-    Flask_App.debug=True
-    Flask_App.run()
+if __name__ == "__main__":
+    app.run(debug=True)
